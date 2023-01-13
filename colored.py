694735a41bd6456478cbd01a6f02f73c6c0db6a1
NeMoMinik <104688335+NeMoMinik@users.noreply.github.com>
2023-01-13 13:29:52 +0300

    Add files via upload

--- colored.py
+++ colored.py
@@ -14,25 +14,32 @@ bridge = CvBridge()
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
 
 hsv_min = np.array((0, 0, 0), np.uint8)
 hsv_max = np.array((1, 1, 1), np.uint8)
 
-
+detected = [False]
 def image_callback(data):
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
     hsv_img = cv.cvtColor(cv_image, cv.COLOR_BGR2HSV)
     thresh = cv.inRange(hsv_img, hsv_min, hsv_max)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    cnt = contours[0]
-    M = cv.moments(cnt)
+    if len(contours) > 0:
+        cnt = contours[0]
+        M = cv.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        detected[:] = True
+        
+
     cv.drawContours(cv_image, contours, -1, (255,0,0), 3, cv.LINE_AA, hierarchy, 1)
     image_pub.publish(bridge.cv2_to_imgmsg(cv_image, 'bgr8'))
 
 def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
     navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown() and not detected:
         telem = get_telemetry(frame_id='navigate_target')
         if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
             break
@@ -50,6 +57,8 @@ navigate_wait(1.2, 3, 2, frame_id='aruco_map')
 navigate_wait(3.2, 3, 2, frame_id='aruco_map')
 navigate_wait(3.2, 1.2, 2, frame_id='aruco_map')
 navigate_wait(1.2, 1.2, 2, frame_id='aruco_map')
+if detected[0]:
+    set_velocity()
 land_wait()
 
 rospy.spin()
